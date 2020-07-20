4678ab52bdc647496703cdf6efc9fd9b43043819
Meha <ahem4progs@gmail.com>
2020-07-20 13:05:20 +0530

    Updated initial code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
-function App() {
+export default function App() {
+  const [count, setCount] = useState(1000);
+
+  function returnValue(inputVal){
+    console.log('returnValue function is called...');
+    return inputVal+10;
+  }
+
+  var calculatedValue = returnValue(10);
+
+  setTimeout(()=>{
+    setCount(count+1);
+  },1000)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello App</h1>
+      <p>You clicked: {count} times.</p>
+      <p>Random value: {calculatedValue} times.</p>
+      <button onClick={()=>{setCount(count+1)}}>Update Count</button>
     </div>
-  );
+  )
+    
 }
-
-export default App;
